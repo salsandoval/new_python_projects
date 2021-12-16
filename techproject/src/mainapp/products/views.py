6cfb00c2aeafc9bd44341145f4bbd3dc4c5a388e
salsandoval <76705210+salsandoval@users.noreply.github.com>
2021-12-16 13:01:15 -0700

    test

--- techproject/src/mainapp/products/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Product
-
-
-def admin_console(request):
-    products = Product.objects.all()
-    return render(request, 'products/product_page.html', {'products': products})
-
